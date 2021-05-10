5f06deafa325b85378e68188f734b81440851dd7
dharuv14 <77673924+dharuv14@users.noreply.github.com>
2021-05-10 12:40:39 +0530

    Add files via upload

---
+++ Plane.js
@@ -0,0 +1,22 @@
+class Plane{
+    constructor(x,y,width,height){
+        var options = {
+isStatic:true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+  display(){
+      var pos = this.body.position;
+     
+      
+      fill("Brown");
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width,this.height);
+      pop();
+  }
+
+
+}
